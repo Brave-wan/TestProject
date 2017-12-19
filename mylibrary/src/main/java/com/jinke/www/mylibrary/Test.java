d2dfe61594aca14d4e98f53566a8ea92ddd8ec60
Jreey <185214487@qq.com>
2017-12-19 17:03:20 +0800

    first commit

---
+++ mylibrary/src/main/java/com/jinke/www/mylibrary/Test.java
@@ -0,0 +1,13 @@
+package com.jinke.www.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by root on 17-12-19.
+ */
+
+public class Test {
+    public static void getString(){
+        Log.i("wan","hello word");
+    }
+}
