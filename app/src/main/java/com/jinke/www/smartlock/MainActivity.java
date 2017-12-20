0ac9a73f77e2ed2c6d3d2ea8ee96f72c8c01e898
Jreey <185214487@qq.com>
2017-12-20 09:17:39 +0800

    添加ｓｄｋ包

--- app/src/main/java/com/jinke/www/smartlock/MainActivity.java
+++ app/src/main/java/com/jinke/www/smartlock/MainActivity.java
@@ -3,7 +3,10 @@ package com.jinke.www.smartlock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dsm.xiaodi.biz.sdk.servercore.ServerUnit;
 import com.jinke.www.mylibrary.Test;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Test.getString();
+        ServerUnit.getInstance().uploadOperatorLog(new ServerUnit.OnServerUnitListener() {
+            @Override
+            public void success(List list, String s) {
+
+
+            }
+
+            @Override
+            public void failure(String s, int i) {
+
+            }
+        });
     }
 }
